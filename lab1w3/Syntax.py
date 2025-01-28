1911bf80a18c83294b3806eead04368cac022da1
RamilKhalmuratov <r_khalmuratov@kbtu.kz>
2025-01-29 02:48:26 +0500

    1

--- lab1w3/Syntax.py
+++ lab1w3/Syntax.py
@@ -1,7 +1,15 @@
 if 5 > 2:
   print("Five is greater than two!")
 
-  if 5 > 2:
+if 5 > 2:
  print("Five is greater than two!") 
 if 5 > 2:
-        print("Five is greater than two!")
+        print("Five is greater than two!") 
+
+#Variables in Python:
+x = 5
+y = "Hello, World!"
+
+#Comments in Python:
+#This is a comment.
+print("Hello, World!")
